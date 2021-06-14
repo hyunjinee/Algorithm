01c44fbbf687676e2c8e66895c95aac0d11d153d
hyunjin <leehj0110@kakao.com>
2021-06-14 15:11:17 +0900

    dfs bfs

---
+++ solved.ac/source/1260.py
@@ -0,0 +1,36 @@
+N,M,V = map(int, input().split())
+
+matrix = [[0] * (N + 1) for i in range(N+1)]
+for i in range(M):
+    a,b = map(int, input().split())
+    matrix[a][b] = matrix[b][a] = 1
+visit_list = [0] * (N + 1)
+
+def dfs(V):
+    visit_list[V] = 1
+    print(V, end=' ')
+    for i in range(1, N+1):
+        if (visit_list[i] == 0 and matrix[V][i] == 1):
+            dfs(i)
+
+    
+
+def bfs(V):
+    queue = [V]
+    visit_list[V] = 0
+
+
+    while queue:
+        V= queue.pop(0)
+
+        print(V, end=" ")
+        
+
+        for i in range(1, N+1):
+            if (visit_list[i] == 1 and matrix[V][i] == 1):
+                queue.append(i)
+                visit_list[i] = 0
+
+dfs(V)
+print()
+bfs(V)
