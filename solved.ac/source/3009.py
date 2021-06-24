bcee9bf1ac7537bbe3b3b9272d856e5e90d080b1
hyunjin <leehj0110@kakao.com>
2021-06-24 16:17:03 +0900

    네 번째 점

---
+++ solved.ac/source/3009.py
@@ -0,0 +1,12 @@
+x_ = []
+y_ = []
+for i in range(3):
+        x, y = map(int, input().split())
+        x_.append(x)
+        y_.append(y)
+for i in range(3):
+        if x_.count(x_[i]) == 1:
+                x = x_[i]
+        if y_.count(y_[i]) == 1:
+                y = y_[i]
+print(x, y)
