88d415c8a6a534045a79b7bda006a495adc0cc86
hyunjin <leehj0110@kakao.com>
2021-06-25 15:59:17 +0900

    명령 프롬프트

--- solved.ac/source/1032.py
+++ solved.ac/source/1032.py
@@ -1,4 +1,12 @@
-a = list(input())
-a_len = len(a)
-print(a_len)
-print(a)
+n = int(input())
+s = list(input())
+for i in range(n-1):
+    new_ =  input()
+    for j in range(len(s)):
+        if s[j] == new_[j]:
+            continue
+        else:
+            s[j] = "?"
+
+
+print("".join(s))
