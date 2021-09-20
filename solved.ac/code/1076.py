c007e3c10de23417d54c4e53af4927f6d770e135
hyunjinee <leehj0110@kakao.com>
2021-09-20 17:19:48 +0900

    저항

---
+++ solved.ac/code/1076.py
@@ -0,0 +1,7 @@
+color = ['black', 'brown', 'red', 
+'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+f = color.index(input())
+s = color.index(input())
+t = color.index(input())
+r = int(str(f) + str(s)) * (10 ** t)
+print(r)
