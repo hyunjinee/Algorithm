9dcd15ea438a0887ff6c937c60c9222bdd9ca007
hyunjinee <leehj0110@kakao.com>
2021-12-17 17:04:36 +0900

    21-12-17 복면산 brute-force

---
+++ solved.ac/source/복면산-brute-force.py
@@ -0,0 +1,14 @@
+w1, w2, w3 = "SEND", "MORE", "MONEY"
+letters = sorted(set(w1 + w2 + w3))
+digits = list(range(10))
+# print(letters, digits)
+
+from itertools import permutations
+perms = list(permutations(digits, len(letters)))
+
+print(len(perms))
+
+for perm in perms:
+  map = {k:v for k, v in zip(letters,perm)}
+
+  print(map)
