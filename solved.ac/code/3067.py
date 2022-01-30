9f1a9453549d44afe7ad915f165d4815eda5bcd9
hyunjin <leehj0110@kakao.com>
2022-01-30 18:37:29 +0900

    22-01-30 Coins knapsack

---
+++ solved.ac/code/3067.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+  n = int(input())
+  coins = list(map(int, input().split()))
+  m = int(input())
+  dp = [0 for _ in range(m+1)]
+
+  dp[0] = 1
+  for i in range(n):
+    for j in range(coins[i], m + 1 ):
+      dp[j] += dp[j-coins[i]]
+  print(dp[m])
+  
