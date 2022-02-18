907f33a59614dcef5833a4fb9bf3d752b93b6e54
hyunjin <leehj0110@kakao.com>
2022-02-19 02:33:37 +0900

    22-02-19 양과 늑대

---
+++ programmers/2022 KAKAO BLIND RECRUITMENT/양과늑대-2.py
@@ -0,0 +1,24 @@
+def solution(info, edges):
+    answer = 0
+    n = len(info)
+    graph = [[] for _ in range(n)]
+    for edge in edges:
+        p,c = edge
+        graph[p].append(c)
+    stack = [(1,0,[0])] # sheep, wolf, visited
+    while stack:
+        cur_sheep, cur_wolf, visited = stack.pop()
+        answer = max(answer, cur_sheep)
+        for cur_node in visited:
+            for next_node in graph[cur_node]:
+                if next_node not in visited:
+                    next_sheep = cur_sheep
+                    next_wolf = cur_wolf
+                    if info[next_node]:
+                        next_wolf += 1
+                    else: 
+                        next_sheep += 1
+                    if next_sheep <= next_wolf:
+                        continue
+                    stack.append((next_sheep, next_wolf, visited+[next_node]))
+    return answer
