99da05b7a2db51734451faa786a1c2411d155884
hyunjin <leehj0110@kakao.com>
2022-03-19 23:50:39 +0900

    chore: clear unused file

--- solved.ac/source/14645.py
+++
@@ -1,5 +0,0 @@
-n, k = map(int, input().split())
-for i in range(n):
-    a,b = map(int, input().split())
-
-print("비와이")
--- solved.ac/source/15596.py
+++
@@ -1,6 +0,0 @@
-def nsum(num_list):
-    result = 0
-    for num in num_list:
-        result += num
-
-    return result
