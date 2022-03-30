f32036e87e9a7f49eb9da2ba04a8a954c950d66d
hyunjin <leehj0110@kakao.com>
2022-03-31 00:31:59 +0900

    chore: delete leetcode folder and rename folder name

--- Leetcode/code/35-2.js
+++
@@ -1,8 +0,0 @@
-var searchInsert = function (nums, target) {
-  if (nums.includes(target)) return nums.indexOf(target)
-  for (let i = 0; i <= nums.length; i++) {
-    if (target < nums[i]) return i
-    else if (target > nums[i] && target < nums[i + 1]) return i + 1
-  }
-  return nums.length
-}
