42cce8742d16eb217318abd4c03fbce24e81a2c7
hyunjin <leehj0110@kakao.com>
2022-05-28 12:39:01 +0900

    22-05-28 다익스트라 공부

---
+++ 코딩 테스트 책/이것이 취업을 위한 코딩테스트다/최단경로/dijkstra2.py
@@ -0,0 +1,51 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
+n, m = map(int, input().split())
+start = int(input())
+
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph =[[] for _ in range(n+1)]
+# 방문한 적이 있는지 체크하는 목적의 리스트를 만들기
+visited = [False] * (n+1)
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n+1)
+
+# 모든 간선 정보를 입력받기
+for _ in range(m):
+  a,b,c = map(int, input().split())
+  graph[a].append((b,c))
+
+
+# 방문하지 않은 노드중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
+def get_smallest_node():
+  min_value = INF
+  index = 0 # 가장 최단 거리가 짧은 노드 (인덱스)
+  for i in range(1, n+1):
+    if distance[i] < min_value and not visited[i]:
+      min_value = distance[i]
+      index = i
+  return index
+
+def dijkstra(start):
+  distance[start] = 0 
+  visited[start] = True
+  for j in graph[start]:
+    distance[j[0]] = j[1]
+  # 시작 노드를 제외한 전체 n-1개의 노드에 대해 반복
+  for i in range(n-1):
+    # 최단 거리가 가장짧은 노드를 꺼내서, 방문처리
+    now = get_smallest_node()
+    visited[now] = True
+    for j in graph[now]:
+      cost = distance[now] + j[1]
+      if cost < distance[j[0]]:
+        distance[j[0]] = cost
+
+dijkstra(start)
+
+for i in range(1, n+1):
+  if distance[i] == INF:
+    print("INFINITY")
+  else:
+    print(distance[i])
