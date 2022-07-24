9018844fe1be83a1f266722f77547bdf85b799c8
hyunjin <leehj0110@kakao.com>
2022-07-24 14:16:13 +0900

    22-07-24 The Game of Death

---
+++ solved.ac/graph_traversal/11558.py
@@ -0,0 +1,15 @@
+def dfs(node):
+  for n in graph[node]:
+      if check[n] == 0:
+          check[n] = check[node]+1
+          dfs(n)
+            
+for _ in range(int(input())):
+    N = int(input())
+    graph = [[] for _ in range(N+1)]
+    for i in range(1, N+1):
+        v = int(input())
+        graph[i].append(v)
+    check = [0]*(N+1)
+    dfs(1)
+    print(check[N] if check[N] > 0 else 0)
