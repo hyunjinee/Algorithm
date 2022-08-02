a9e194d983713da128b7c70917c971412b05b02f
hyunjin <leehj0110@kakao.com>
2022-08-02 22:42:24 +0900

    22-08-02 특정 거리의 도시 찾기

---
+++ solved.ac/graph_traversal/18352.py
@@ -0,0 +1,45 @@
+from collections import deque
+import sys
+
+f = sys.stdin.readline
+
+n,m,k,x =map(int, f().split())
+
+graph =[[  ] for _ in range(n+1)]
+
+distance = [0] + (n + 1)
+
+visited = [False] * (n + 1)
+
+for _ in range(m):
+  a,b = map(int, f().split())
+  graph[a].append(b)
+
+def bfs(start):
+  answer =[]
+  q= deque([start])
+
+  visited[start] =True
+  distance[start] = 0
+
+  while q:
+    now = q.popleft()
+    for i in graph[now]:
+      if not visited[i]:
+        visited[i] = True
+        distance[i] = distance[now] + 1
+        q.append(i)
+        
+        if distance[i] == k:
+          answer.append(i)
+  if len(answer) == 0:
+    print(-1)
+
+  
+  else: 
+    answer.sort()
+
+    for i in answer: 
+      print( i , end='\n')
+
+bfs(x)
