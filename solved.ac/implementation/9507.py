853cbe0971a6ab2623309d4691e280bf8b3dc5b9
hyunjin <leehj0110@kakao.com>
2022-09-12 19:37:00 +0900

    22-09-12 Generations of Tribbles

---
+++ solved.ac/implementation/9507.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+
+dp = [0] * 68
+dp[0] = 1
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for i in range(4, 68):
+  dp[i] = dp[i-1] + dp[i-2] + dp[i-3] + dp[i-4]
+
+for _ in range(int(input())):
+  n = int(input())
+
+  print(dp[n])
