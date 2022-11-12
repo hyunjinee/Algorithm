fd1b907d45eab16102ccdc3a6b51577ee509e994
hyunjin <leehj0110@kakao.com>
2022-11-13 00:22:56 +0900

    22-11-13 1,2,3 더하기 4

---
+++ solved.ac/implementation/15989.py
@@ -0,0 +1,11 @@
+t = int(input())
+dp = [1]* 10001
+for i in range(2, 10001):
+    dp[i] += dp[i - 2]
+    
+for i in range(3, 10001):
+    dp[i] += dp[i - 3]
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
