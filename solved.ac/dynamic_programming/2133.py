8bb772bb2eae9befb32ec4d1048c0db819dd2217
hyunjin <leehj0110@kakao.com>
2022-12-29 19:43:56 +0900

    22-12-29 타일 채우기

---
+++ solved.ac/dynamic_programming/2133.py
@@ -0,0 +1,9 @@
+n = int(input())
+dp = [0 for _ in range(31)]
+dp[2] = 3
+for i in range(4,n+1) :
+    if i%2 == 0 :
+        dp[i] = dp[i-2] * 3 + sum(dp[:i-2]) * 2 + 2
+    else :
+        dp[i] = 0
+print(dp[n])
