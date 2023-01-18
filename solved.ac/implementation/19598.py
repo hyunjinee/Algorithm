462c34567d9a39fc346163d5144cff905b8f9694
hyunjin <leehj0110@kakao.com>
2023-01-18 16:45:03 +0900

    22-01-18 최소 회의실 개수

---
+++ solved.ac/implementation/19598.py
@@ -0,0 +1,21 @@
+import sys
+import heapq
+input = sys.stdin.readline
+ 
+N = int(input())
+arr = []
+for _ in range(N):
+    arr.append(list(map(int, input().split())))
+arr.sort(key=lambda x: x[0])
+ 
+rooms = [0]
+answer = 1
+for s, e in arr:
+    if s >= rooms[0]:
+        heapq.heappop(rooms)
+    else:
+        answer+=1
+    heapq.heappush(rooms, e)
+    
+    print(rooms)
+print(answer)
