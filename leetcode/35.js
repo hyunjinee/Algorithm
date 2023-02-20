45180235a0bc7053392ceed8fa2b939f2920b45b
hyunjin <leehj0110@kakao.com>
2023-02-20 22:46:18 +0900

    22-02-20 search insert position

--- leetcode/35.js
+++ leetcode/35.js
@@ -1,18 +1,27 @@
-var searchInsert = function (nums, target) {
-  let start = 0
-  let end = nums.length - 1
-  let mid = Math.floor((end - start) / 2)
-
-  while (start <= end) {
-    mid = Math.floor((start + end) / 2)
-    if (nums[mid] == target) {
-      return mid
-    }
-    if (nums[mid] > target) {
-      end = mid - 1
-    } else {
-      start = mid + 1
-    }
+const searchInsert = function (nums, target) {
+  if (nums.includes(target)) return nums.indexOf(target)
+  for (let i = 0; i <= nums.length; i++) {
+    if (target < nums[i]) return i
+    else if (target > nums[i] && target < nums[i + 1]) return i + 1
   }
-  return start
+  return nums.length
 }
+
+// var searchInsert = function (nums, target) {
+//   let start = 0
+//   let end = nums.length - 1
+//   let mid = Math.floor((end - start) / 2)
+
+//   while (start <= end) {
+//     mid = Math.floor((start + end) / 2)
+//     if (nums[mid] == target) {
+//       return mid
+//     }
+//     if (nums[mid] > target) {
+//       end = mid - 1
+//     } else {
+//       start = mid + 1
+//     }
+//   }
+//   return start
+// }
