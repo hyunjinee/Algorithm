adba552630b1a552b142052e863cace8c0848da4
hyunjin <leehj0110@kakao.com>
2023-04-14 11:34:33 +0900

    23-04-14 map 기본 다루기

--- README.md
+++ README.md
@@ -100,7 +100,3 @@ const putComma = (s) => {
 ```
 
 [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fhyunjinee%2FAlgorithm&count_bg=%23262382&title_bg=%233E2ABA&icon=mediafire.svg&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
-
-```
-
-```
---
+++ programmers/연습문제/구현/맵.js
@@ -0,0 +1,20 @@
+const map = new Map()
+
+map.set("name", "crong")
+map.set("age", 10)
+
+console.log(map.keys())
+
+for (let key of map.keys()) {
+  console.log(key)
+}
+
+for (let value of map.values()) {
+  console.log(value)
+}
+
+for (let [a, b] of map.entries()) {
+  console.log(a, b)
+}
+
+console.log(NaN !== NaN)
