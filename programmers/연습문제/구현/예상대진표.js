eca1b4ae8b2195076b7dd1839b042ff4f55c7d97
hyunjin <leehj0110@kakao.com>
2023-04-17 20:37:11 +0900

    23-04-17 예상 대진표

---
+++ programmers/연습문제/구현/예상대진표.js
@@ -0,0 +1,12 @@
+function solution(n, a, b) {
+  let answer = 0
+
+  while (a !== b) {
+    a = Math.ceil(a / 2)
+    b = Math.ceil(b / 2)
+    // console.log(a, b)
+    answer++
+  }
+
+  return answer
+}
