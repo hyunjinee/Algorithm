fb2c3693046cbbf328a15d462b8bd8488a4d2583
hyunjin <leehj0110@kakao.com>
2023-05-01 10:38:20 +0900

    23-05-01 Length of LIS

---
+++ leetcode/300.js
@@ -0,0 +1,13 @@
+function lengthOfLIS(nums) {
+  const dp = Array(nums.length).fill(1)
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1)
+      }
+    }
+  }
+
+  return Math.max(...dp)
+}
